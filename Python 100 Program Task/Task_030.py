03ae9cc6c8ad7387b725894f859f36d6a6799bf4
JIGNESHGM <jigneshmevada87@gmail.com>
2025-04-30 17:14:55 +0530

    Task 30

--- Python 100 Program Task/Task_030.py
+++ Python 100 Program Task/Task_030.py
@@ -1,3 +1,27 @@
-# This is Task_030.py
-# Python script created automatically
-print('Hello from task script!')
+# program 30 :
+
+# Define a function that can accept two strings
+# as input and concatenate them and then print it in console.
+
+# Hints:
+# Use + to concatenate the strings
+
+# Function Create
+def concatenate_strings(str1, str2):
+    return str1 + " " + str2
+    
+# Main Function
+def main():
+    # Input from user
+    string1 = input("Enter first string: ")
+    string2 = input("Enter second string: ")
+    
+    # Call the function to concatenate and print
+    result = concatenate_strings(string1, string2)
+    
+    #Print Result 
+    print("Your String Concatenate after : \n {0}".format(result))
+    
+# Call the main function
+if __name__ == "__main__":
+    main()
--- Python 100 Program Task/Task_031.py
+++ Python 100 Program Task/Task_031.py
@@ -1,3 +1,9 @@
-# This is Task_031.py
-# Python script created automatically
-print('Hello from task script!')
+# program 31 :
+
+# Define a function that can accept two strings as input
+# and print the string with maximum length in console.
+# If two strings have the same length, then the function
+# should print all strings line by line.
+
+# Hints:
+# Use len() function to get the length of a string
