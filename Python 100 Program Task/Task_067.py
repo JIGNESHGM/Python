7ed11463f9d8898fcfaf05ccfad4e017efa0014f
JIGNESHGM <jigneshmevada87@gmail.com>
2025-05-01 16:11:49 +0530

    Task 67

--- Python 100 Program Task/Task_067.py
+++ Python 100 Program Task/Task_067.py
@@ -27,3 +27,30 @@
 # In case of input data being supplied to the question,
 # it should be assumed to be a console input.
 
+# Function to compute the Fibonacci sequence up to n using iteration
+def fibonacci_sequence(n):
+    if n == 0:
+        return [0]
+    elif n == 1:
+        return [0, 1]
+    
+    fib_sequence = [0, 1]
+    for i in range(2, n + 1):
+        fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+    return fib_sequence
+
+# Main function to test the fibonacci_sequence function
+def main():
+    try:
+        n = int(input("Enter a non-negative integer n: "))
+        if n >= 0:
+            result = fibonacci_sequence(n)
+            print(','.join(map(str, result)))
+        else:
+            print("Please enter a non-negative integer.")
+    except ValueError:
+        print("Invalid input. Please enter a valid non-negative integer.")
+
+# Program starting point 
+if __name__ == '__main__':
+    main()
