27c3e64fabcf6fe8b995d3f8546301a1352ac77a
JIGNESHGM <jigneshmevada87@gmail.com>
2025-05-01 16:17:19 +0530

    Task 69

--- Python 100 Program Task/Task_069.py
+++ Python 100 Program Task/Task_069.py
@@ -18,3 +18,25 @@
 
 # In case of input data being supplied to the question,
 # it should be assumed to be a console input.
+
+# Function to generate numbers divisible by 5 and 7 up to n using a generator
+def divisible_by_5_and_7_generator(n):
+    for i in range(0, n + 1):
+        if i % 5 == 0 and i % 7 == 0:
+            yield i
+            
+# Main function to test the divisible_by_5_and_7_generator function
+def main():
+    try:
+        n = int(input("Enter a non-negative integer n: "))
+        if n >= 0:
+            divisible_numbers = divisible_by_5_and_7_generator(n)
+            print(','.join(map(str, divisible_numbers)))
+        else:
+            print("Please enter a non-negative integer.")
+    except ValueError:
+        print("Invalid input. Please enter a valid non-negative integer.")
+        
+# Program starting point
+if __name__ == '__main__':
+    main()
