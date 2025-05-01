e0221452eb7a15ff1de8615d63ff8fac759ad157
JIGNESHGM <jigneshmevada87@gmail.com>
2025-05-01 15:58:03 +0530

    Task 59

--- Python 100 Program Task/Task_059.py
+++ Python 100 Program Task/Task_059.py
@@ -19,3 +19,21 @@
 
 # Hints:
 # Use \w to match letters.
+
+# Function to extract the company name from an email address
+def extract_company_name(email):
+    # Split the email address at the '@' symbol and return the second part (company name without domain)
+    return email.split('@')[1].split('.')[0]
+
+# Main function to test the extract_company_name function
+def main():
+    # Input email address from the user
+    email = input("Enter an email address (username@companyname.com): ")
+    
+    company_name = extract_company_name(email)
+    print("Company name:", company_name)
+
+
+# Call the main function to execute the program
+if __name__ == "__main__":
+    main()s
