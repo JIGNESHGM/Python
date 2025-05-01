b352cf0732c2c5a65bd4f3631a23765d7ffec558
JIGNESHGM <jigneshmevada87@gmail.com>
2025-05-01 16:12:42 +0530

    Task 68

--- Python 100 Program Task/Task_068.py
+++ Python 100 Program Task/Task_068.py
@@ -18,3 +18,24 @@
 
 # In case of input data being supplied to the question,
 # it should be assumed to be a console input.
+
+# Function to generate even numbers up to n using a generator
+def even_numbers_generator(n):
+    for i in range(0, n + 1, 2):
+        yield i
+        
+# Main function to test the even_numbers_generator function
+def main():
+    try:
+        n = int(input("Enter a non-negative integer n: "))
+        if n >= 0:
+            even_numbers = even_numbers_generator(n)
+            print(','.join(map(str, even_numbers)))
+        else:
+            print("Please enter a non-negative integer.")
+    except ValueError:
+        print("Invalid input. Please enter a valid non-negative integer.")
+        
+# Program starting point
+if __name__ == '__main__':
+    main()
