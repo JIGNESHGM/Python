968d167368464a2063926d213ab68934d27bc5e4
JIGNESHGM <jigneshmevada87@gmail.com>
2025-05-01 16:04:37 +0530

    Task 63

--- Python 100 Program Task/Task_063.py
+++ Python 100 Program Task/Task_063.py
@@ -1,3 +1,7 @@
 # program 63:
 # Write a special comment to indicate a Python source code file is in unicode.
 
+# Hints:
+# Use # -*- coding: utf-8 -*- at the beginning of the file.
+
+# -*- coding: utf-8 -*-
