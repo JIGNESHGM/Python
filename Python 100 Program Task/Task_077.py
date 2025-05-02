d52ea3146ed2e0ba3c3e39e13c2f13fa33fa05e1
JIGNESHGM <jigneshmevada87@gmail.com>
2025-05-02 16:55:56 +0530

    Task 77,78

--- Python 100 Program Task/Task_077.py
+++ Python 100 Program Task/Task_077.py
@@ -6,3 +6,25 @@
 
 # Hints:
 # Use random.choice() to a random element from a list.
+
+# Function to generate a random number divisible by 5 and 7 between 0 and 10 inclusive
+import random
+
+def random_divisible_number(start, end):
+    # Generate a list of numbers divisible by 5 and 7 between 0 and 10 inclusive
+    divisible_numbers = [num for num in range(start, end) if num % 5 == 0 and num % 7 == 0]
+    
+    # Choose a random number from the list
+    return random.choice(divisible_numbers)
+
+# Main function to execute the program
+def main():
+    
+    start = int(input("Enter the start of the range (inclusive): "))
+    end = int(input("Enter the end of the range (exclusive): "))
+    random_divisible = random_divisible_number(start, end)
+    print(f"Random number between {start} and {end} inclusive divisible by 5 and 7: {random_divisible}")
+    
+# Call the main function to run the program
+if __name__ == "__main__":
+    main()
--- Python 100 Program Task/Task_078.py
+++ Python 100 Program Task/Task_078.py
@@ -5,3 +5,22 @@
 
 # Hints:
 # Use random.sample() to generate a list of random values.
+
+# Function to generate a list of 5 random numbers between 100 and 200 inclusive
+import random
+def generate_random_numbers(start, end, count):
+    # Generate a list of random numbers between 100 and 200 inclusive
+    random_numbers = random.sample(range(start, end + 1), count)
+    return random_numbers
+
+# Main function to execute the program
+def main():
+    start = int(input("Enter the start of the range (inclusive): "))
+    end = int(input("Enter the end of the range (inclusive): "))
+    count = int(input("Enter the number of random numbers to generate: "))
+    random_numbers = generate_random_numbers(start, end, count)
+    print(f"List of {count} random numbers between {start} and {end} inclusive: {random_numbers}")
+    
+# Call the main function to run the program
+if __name__ == "__main__":
+    main()
