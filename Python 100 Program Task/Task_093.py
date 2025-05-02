996f5e43ca6acfc76737c5d8f90fc8fd4796ea0b
JIGNESHGM <jigneshmevada87@gmail.com>
2025-05-02 17:23:29 +0530

    Task 93

--- Python 100 Program Task/Task_093.py
+++ Python 100 Program Task/Task_093.py
@@ -6,3 +6,25 @@
 
 # Hints:
 # Use set() and "&=" to do set intersection operation.
+
+# Function to find the intersection of two lists
+def find_intersection(list1, list2):
+    # Convert lists to sets and find the intersection
+    intersection = set(list1) & set(list2)
+    return list(intersection)
+
+# Main function to execute the program
+def main():
+    # Given lists
+    list1 = [int(x) for x in input("Enter the first list of numbers separated by spaces: ").split()]
+    list2 = [int(x) for x in input("Enter the second list of numbers separated by spaces: ").split()]
+    
+    # Find the intersection of the two lists
+    result_list = find_intersection(list1, list2)
+    
+    # Print the result
+    print("Intersection of the two lists:", result_list)
+    
+# Call the main function to run the program
+if __name__ == "__main__":
+    main()
